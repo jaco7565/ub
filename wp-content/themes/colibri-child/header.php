3ca511a31a416b560b976691d59583317411f45e
jaco7565 <jaco7565@stud.kea.dk>
2021-11-04 09:20:23 +0100

    first commit

---
+++ wp-content/themes/colibri-child/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel ="stylesheet" href ="style.css">
+    <?php wp_head(); ?>
+    <?php colibriwp_theme()->get( 'css' )->render(); ?>
+    
+</head>
+
+<body id="colibri" <?php body_class(); ?>>
+<p>Hej med dig</p>
+<?php
+if ( function_exists( 'wp_body_open' ) ) {
+    wp_body_open();
+} else {
+    do_action( 'wp_body_open' );
+}
+?>
+<div class="site" id="page-top">
+    <?php colibriwp_theme()->get( 'header' )->render(); ?>
+
